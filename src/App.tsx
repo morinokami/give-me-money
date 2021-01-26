5333132e229825cdf9afa5b0f5dd208420478a20
Shinya Fujino <shf0811@gmail.com>
2021-01-27 01:27:45 +0900

    Implement the client app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+import CheckoutForm from './CheckoutForm';
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_51IDte5Bv64yFsKT9pnhoJow9WOVeXvCLVrqxnzYUeGbiagl1atskKt5MuIEkWFJgPD98aSs4t3QRIQj7A6FGBPlD00FnrSnEnOO');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
+    </Elements>
   );
-}
+};
 
 export default App;
---
+++ src/CheckoutForm.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
+
+import CardSection from './CardSection';
+
+export default function CheckoutForm() {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    // We don't want to let default form submission happen here,
+    // which would refresh the page.
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    const response = await fetch('/.netlify/functions/secret', {
+      method: 'POST',
+      body: JSON.stringify({ amount: 888 }), // TODO
+      headers: { 'Content-Type': 'application/json' }
+    })
+    const data = await response.json()
+    const secret = data.client_secret
+
+    const result = await stripe.confirmCardPayment(secret, {
+      payment_method: {
+        card: elements.getElement(CardElement)!,
+        billing_details: {
+          name: 'Jenny Rosen',
+        },
+      }
+    });
+
+    if (result.error) {
+      // Show error to your customer (e.g., insufficient funds)
+      console.log(result.error.message);
+    } else {
+      // The payment has been processed!
+      if (result.paymentIntent?.status === 'succeeded') {
+        // Show a success message to your customer
+        // There's a risk of the customer closing the window before callback
+        // execution. Set up a webhook or plugin to listen for the
+        // payment_intent.succeeded event that handles any business critical
+        // post-payment actions.
+        console.log('succeeded')
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardSection />
+      <button disabled={!stripe}>Confirm order</button>
+    </form>
+  );
+}
