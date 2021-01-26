11042ee6f871d90a87c80522859b4ac246f961d5
Shinya Fujino <shf0811@gmail.com>
2021-01-27 01:37:30 +0900

    Update the publishable key

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import CheckoutForm from './CheckoutForm';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_51IDte5Bv64yFsKT9pnhoJow9WOVeXvCLVrqxnzYUeGbiagl1atskKt5MuIEkWFJgPD98aSs4t3QRIQj7A6FGBPlD00FnrSnEnOO');
+const stripePromise = loadStripe('pk_test_51IDte5Bv64yFsKT9pnhoJow9WOVeXvCLVrqxnzYUeGbiagl1atskKt5MuIEkWFJgPD98aSs4t3QRIQj7A6FGBPlD00FnrSnEnO');
 
 function App() {
   return (
